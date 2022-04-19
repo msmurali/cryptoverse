e592b31e0472e0ccc7ebc926351ffd8b3e103711
msmurali <muralikrishnan0511@gmail.com>
2022-04-19 15:19:46 +0530

    create card.jsx

--- src/pages/home_page.jsx
+++ src/pages/home_page.jsx
@@ -1,20 +1,25 @@
 import React from "react";
 import Header from "../components/header";
+import Card from "../components/card";
 import { formatNum } from "../services/utils/helpers";
 
 const HomePage = ({ crypto }) => {
   const { data: cryptoData, status: cryptoStatus, error: cryptoErr } = crypto;
-  // // const { newsData, newsStatus, newsErr } = news;
+  // const { newsData, newsStatus, newsErr } = news;
 
   return (
     <React.Fragment>
       <Header />
-      {cryptoStatus === "done" ? (
+      {/* {cryptoStatus === "done" ? (
         <CryptoStats stats={cryptoData.stats} />
       ) : (
         <></>
       )}
-      <Card />
+      {cryptoStatus === "done" ? (
+        <CryptoCurrencies coins={cryptoData.coins} />
+      ) : (
+        <></>
+      )} */}
     </React.Fragment>
   );
 };
@@ -70,8 +75,40 @@ const CryptoStats = ({ stats }) => {
   );
 };
 
-const Card = () => {
-  return <div className="card w-80 h-60"></div>;
+const CryptoCurrencies = ({ coins }) => {
+  return (
+    <div className="mx-4 md:mx-6 p-4 md:p-8">
+      <h1 className="text-white font-medium mb-6 md:text-xl font-primary">
+        Crypto Currencies
+      </h1>
+      <div className="flex justify-between flex-wrap items-start">
+        {coins.map((coin) => (
+          <Card key={coin.uuid}>
+            <div className="w-full flex justify-between items-center mb-4">
+              <h1 className="font-medium capitalize text-secondary text-lg">
+                {coin.name}
+              </h1>
+              <img src={coin.iconUrl} alt={coin.name} className="w-14 h-14" />
+            </div>
+            <div className="flex flex-col justify-evenly">
+              <div className="w-full flex justify-between items-center">
+                <h1 className="capitalize">price:</h1>
+                <p className="">{formatNum(coin.price)}</p>
+              </div>
+              <div className="w-full flex justify-between items-center">
+                <h1 className="capitalize">market cap:</h1>
+                <p className="">{formatNum(coin.marketCap)}</p>
+              </div>
+              <div className="w-full flex justify-between items-center">
+                <h1 className="capitalize">24h volume:</h1>
+                <p className="">{formatNum(coin["24hVolume"])}</p>
+              </div>
+            </div>
+          </Card>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
