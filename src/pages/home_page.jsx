d64bb7e489baae136a69b4db185cf43315d3fa57
msmurali <muralikrishnan0511@gmail.com>
2022-04-19 16:47:50 +0530

    update home_page.jsx

--- src/pages/home_page.jsx
+++ src/pages/home_page.jsx
@@ -3,13 +3,14 @@ import Header from "../components/header";
 import Card from "../components/card";
 import { formatNum } from "../services/utils/helpers";
 import Loader from "../components/loader";
+import Logo from "../asset/icons/logo.svg";
 
 const HomePage = ({ crypto, news }) => {
   const { data: cryptoData, status: cryptoStatus, error: cryptoErr } = crypto;
-  const { newsData, newsStatus, newsErr } = news;
+  const { data: newsData, status: newsStatus, error: newsErr } = news;
 
   return (
-    <React.Fragment>
+    <div className="md:px-4">
       <Header />
       {cryptoStatus !== "done" ? (
         <Loader />
@@ -19,7 +20,12 @@ const HomePage = ({ crypto, news }) => {
           <CryptoCurrencies coins={cryptoData.coins} />
         </React.Fragment>
       )}
-    </React.Fragment>
+      {newsStatus !== "done" ? (
+        <Loader />
+      ) : (
+        <CryptoNews news={newsData.value} />
+      )}
+    </div>
   );
 };
 
@@ -108,6 +114,44 @@ const CryptoCurrencies = ({ coins }) => {
             </div>
           </Card>
         ))}
+      </div>
+      <span className="font-medium cursor-pointer block text-sm font-primary text-secondary text-center mx-auto my-6 md:hidden">
+        SHOW MORE
+      </span>
+    </div>
+  );
+};
+
+const CryptoNews = ({ news }) => {
+  return (
+    <div className="mx-4 md:mx-6 p-4 md:p-8">
+      <h1 className="text-white font-medium mb-6 md:text-xl font-primary flex justify-between items-center">
+        Crypto News
+        <span className="font-medium cursor-pointer text-sm font-primary text-secondary hidden md:inline">
+          SHOW MORE
+        </span>
+      </h1>
+      <div className="flex justify-between flex-wrap items-start">
+        {news.slice(0, 12).map((news) => {
+          return (
+            <Card key={news.datePublished}>
+              <div className="w-full flex justify-between items-center mb-4">
+                <h1 className="font-medium capitalize text-secondary text-lg">
+                  {`${news.name.slice(0, 40)}...`}
+                </h1>
+                <img
+                  src={news?.image?.thumbnail?.contentUrl || Logo}
+                  alt="thumbnail"
+                  className="w-14 h-14"
+                />
+              </div>
+              <div className="flex flex-col justify-evenly">
+                <p className="">{`${news.description.slice(0, 60)}...`}</p>
+                <p>{news.provider[0].name}</p>
+              </div>
+            </Card>
+          );
+        })}
       </div>
       <span className="font-medium cursor-pointer block text-sm font-primary text-secondary text-center mx-auto my-6 md:hidden">
         SHOW MORE
